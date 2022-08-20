01865167a48028b3dbb424f56f059ee9d90236c4
robin-cybrosys <robin.cybrosys@gmail.com>
2022-08-20 15:15:30 +0530

    Aug:20:latest courses: carousal finalised

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,12 @@
-# Create a snippet to show latest 3 elearning courses. On click it should
-# redirect to corresponding course. Dynamic snippet which shows elearning
-# courses.
 {
     'name': "E-Learning Snippet",
     'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'website': "http://www.cybrosys.com",
     'category': 'Uncategorized',
     'version': '15.0.1.0.0',
     'depends': ['base','web','website_slides'],
     'data': [
         'snippets/elearning_snippet.xml',
-        # 'snippets/elearning_template.xml',
     ],
     'assets': {
         'web.assets_frontend': [
